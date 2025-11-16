71976be451f168b3669ef52bfc22fd1403430440
MustafaBioS <bioshpere.voices@gmail.com>
2025-11-16 16:27:10 +0200

    ADD - Better textures and atmosphere

---
+++ static/glow.js
@@ -0,0 +1,7 @@
+import * as THREE from "three";
+
+function getGlow({ rimHex = 0x0088ff, facingHex }) {
+    const uniforms = {
+        color1: {}
+    }
+}
--- static/space.js
+++ static/space.js
@@ -16,7 +16,7 @@ const scene = new THREE.Scene();
 const loader = new THREE.TextureLoader();
 const geometry = new THREE.IcosahedronGeometry(1, 12);
 const material = new THREE.MeshStandardMaterial({
-    map: loader.load("static/maps/earthmap1k.jpg"),
+    map: loader.load("static/maps/8k_earth_daymap.jpg"),
 })
 
 const earthGroup = new THREE.Group();
@@ -27,11 +27,34 @@ const earthMesh = new THREE.Mesh(geometry, material);
 earthGroup.add(earthMesh);
 
 const lightMat = new THREE.MeshBasicMaterial({ 
-    map: loader.load("static/maps/earthlights1k.jpg"),
+    map: loader.load("static/maps/8k_earth_nightmap.jpg"),
     blending: THREE.AdditiveBlending,
+    transparent: true,
+    opacity: 0.6
 })
 const lightMesh = new THREE.Mesh(geometry, lightMat)
+lightMesh.scale.setScalar(1.001);
 earthGroup.add(lightMesh);
+
+const cloudsMat = new THREE.MeshBasicMaterial({
+    map: loader.load("static/maps/8k_earth_clouds.jpg"),
+    transparent: true,
+    opacity: 0.4,
+    depthWrite: false
+})
+
+const cloudsMesh = new THREE.Mesh(geometry, cloudsMat);
+cloudsMesh.scale.setScalar(1.002);
+earthGroup.add(cloudsMesh);
+
+const atmoMat = new THREE.MeshPhongMaterial({
+    color: 0x00aaff,
+    side: THREE.BackSide,
+    opacity: 0.1,
+});
+const atmoGeo = new THREE.SphereGeometry(1.02, 64, 64);
+const atmosphere = new THREE.Mesh(atmoGeo, atmoMat);
+earthGroup.add(atmosphere);
 
 const stars = getStars();
 scene.add(stars);
@@ -47,6 +70,8 @@ function animate() {
     
     earthMesh.rotation.y += 0.002;
     lightMesh.rotation.y += 0.002;
+    cloudsMesh.rotation.y += 0.0025;
+
     renderer.render(scene, cam);
 }
 animate();
