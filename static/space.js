dbe7641c2824604978cabe0b62432207cae9c7c4
MustafaBioS <bioshpere.voices@gmail.com>
2025-11-16 14:15:30 +0200

    ADD - Earth Planet

--- static/space.js
+++ static/space.js
@@ -1,19 +1,40 @@
 import * as THREE from "three";
+import { OrbitControls } from "jsm/controls/OrbitControls.js";
 
 const h = window.innerHeight;
 const w = window.innerWidth;
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(w, h)
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
-const fov = 75;
-const aspect = w / h;
-const near = 0.1;
-const far = 10;
-const cam = new THREE.PerspectiveCamera(fov, aspect, near, far)
-cam.position.z = 2;
+const cam = new THREE.PerspectiveCamera(75, w / h, 0.1, 100);
+cam.position.z = 3;
 
 const scene = new THREE.Scene();
+const loader = new THREE.TextureLoader();
+const geometry = new THREE.IcosahedronGeometry(1, 12);
+const material = new THREE.MeshStandardMaterial({
+    map: loader.load("static/maps/earthmap1k.jpg"),
+})
 
+const earthGroup = new THREE.Group();
+earthGroup.rotation.z = -23.4 * Math.PI / 180;
+scene.add(earthGroup)
 
-renderer.render(scene, cam);
+const earthMesh = new THREE.Mesh(geometry, material);
+
+scene.add(earthMesh);
+
+const hemilight = new THREE.HemisphereLight(0xffffff, 0x444444);
+scene.add(hemilight)
+
+const controls = new OrbitControls(cam, renderer.domElement);
+
+function animate() {
+    requestAnimationFrame(animate);
+    
+    earthMesh.rotation.y += 0.002;
+    renderer.render(scene, cam);
+}
+animate();
